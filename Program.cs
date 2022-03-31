f988ce1826eff7272588617acc2a27906e086120
VK85111 <aradel3d@gmailc.om>
2022-03-31 21:57:55 +0300

    Проект оптимизирован

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Text;
-using System.Data.SqlClient;
-using System.Configuration;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text.Json;
+using SQL_Solution.Model;
+using SQL_Solution.SQLToolsNameSpace;
 
 namespace RabbitMQ_simple_SQL_Solution
 {
@@ -13,10 +13,9 @@ namespace RabbitMQ_simple_SQL_Solution
         public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost", UserName = "Keks", Password = "Kokoko" };
-            //Как хранить эту строку с настройками в App.config?
-            using (var connection = factory.CreateConnection()) //Простыми словами в понятиях 1С про using?
-            using (var channel = connection.CreateModel()) //Я так понял это из-за connection в котором сидит channel
-            //Почему не просто var channel = connection.CreateModel() в стиле 1С?
+            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
             {
                 channel.QueueDeclare(queue: "FM",
                                      durable: true,
@@ -25,123 +24,28 @@ namespace RabbitMQ_simple_SQL_Solution
                                      arguments: null);
 
                 var consumer = new EventingBasicConsumer(channel);
-                //почему тип не задали для model, ea?
+                
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
 
-                    var rootobjectjson = new Rootobject();
-                    rootobjectjson = JsonSerializer.Deserialize<Rootobject>(message);
-                    //Console.WriteLine(Convert.ToDateTime(rootobjectjson.timestamp));//Конвертация теперь в классе
+                    var rootobjectjson = new RootObject();
+                    rootobjectjson = JsonSerializer.Deserialize<RootObject>(message);
                     
-                    //string[] arrayToSQL = new string[4];//Как сделать массив с разными типами внутри?
-                    ////Видимо никак
-                    //arrayToSQL[0] = rootobjectjson.action;
-                    //arrayToSQL[1] = "tmstmp";//rootobjectjson.timestamp; 
-                    //arrayToSQL[2] = rootobjectjson.data.leasingcalculation;
-                    //arrayToSQL[3] = "5000";//rootobjectjson.data.summary;
-
-                    SQL_tools.DeleteFromMS_SQL(rootobjectjson);
-                    SQL_tools.SendToMS_SQL(rootobjectjson);
+                    SQLTools.DeleteFromMS_SQL(rootobjectjson);
+                    //Можно обращаться прямо, без using в шапке
+                    SQL_Solution.SQLToolsNameSpace.SQLTools.SendToMS_SQL(rootobjectjson);
 
                 };
 
                 channel.BasicConsume(queue: "FM", autoAck: true, consumer: consumer);
-
-                //Console.WriteLine(consumer.Model); //а где ea и почему M у Model большая?
-                
+ 
                 Console.ReadLine();
 
             }
 
         }
-        //Имеет ли смысл вывести эти сущности (типа метаданные справочники) в отдельные *.cs файлы? Как принято?
-        public class Rootobject
-        {
-            public DateTime timestamp { get; set; }
-            public string action { get; set; }
-            public string model { get; set; }
-            public Data data { get; set; }
-            public string comment { get; set; }
 
-        }
-
-        public class Data
-        {
-            public string GUID { get; set; }
-            public string number { get; set; }
-            public string leasingcalculation { get; set; }
-            public DateTime date { get; set; }
-            public string bldblp { get; set; }
-            public decimal summary { get; set; }
-
-        }
-
-        public static class SQL_tools //Может это уже новый Namespace? Типа своя подсистема.
-        {
-            public static void DeleteFromMS_SQL(Rootobject args)
-            {   
-                //Копипаста, вывести в отдельную функцию
-                SqlConnection sqlConnection = null;
-                sqlConnection = new SqlConnection(GetConnectionStringByName("SQL_URL"));
-                sqlConnection.Open();
-
-                SqlCommand sqlCommand = new SqlCommand("DELETE FROM [FM_data] WHERE[GUID] = @GUID", sqlConnection);
-                sqlCommand.Parameters.AddWithValue("@GUID", args.data.GUID);
-                sqlCommand.ExecuteNonQuery();
-
-                sqlConnection.Close();// Что будет если не закрыть?
-
-            }
-            public static void SendToMS_SQL(Rootobject args)
-            {
-                SqlConnection sqlConnection = null;
-                sqlConnection = new SqlConnection(GetConnectionStringByName("SQL_URL"));
-                sqlConnection.Open();
-
-                //Получить объяснение NuGet package, почему так по разному подключаются библиотеки?
-
-                //SqlCommand sqlCommand0 = new SqlCommand("DELETE FROM [FM_data] WHERE[GUID] = @GUID", sqlConnection);
-                ////Почему не переопределилась sqlCommand?
-                ////Можно ли сделать одну сущность sqlCommand, но менять только текст запроса? Чтоб параметры остались.
-                ////Я так понял это будет sqlCommand.CommandText = "";
-                //sqlCommand0.Parameters.AddWithValue("@GUID", args.data.GUID);
-                //sqlCommand0.ExecuteNonQuery(); //Альтернативы этого NonQuery?
-
-                SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [FM_data]([GUID], [action], [timestamp], [number], " +
-                    $"[leasingcalculation], [date], [bldblp], [summary]) " +
-                    $"VALUES (@GUID, @action, @timestamp, @number, @leasingcalculation, @date, @bldblp, @summary)", sqlConnection);
-                //sqlCommand.Parameters.AddRange(parameters); //Нужен пример этого исполнения
-                sqlCommand.Parameters.AddWithValue("@GUID", args.data.GUID);
-                sqlCommand.Parameters.AddWithValue("@action", args.action);
-                sqlCommand.Parameters.AddWithValue("@timestamp", args.timestamp);
-                sqlCommand.Parameters.AddWithValue("@leasingcalculation", args.data.leasingcalculation);
-                sqlCommand.Parameters.AddWithValue("@number", args.data.number);
-                sqlCommand.Parameters.AddWithValue("@date", args.data.date);
-                sqlCommand.Parameters.AddWithValue("@bldblp", args.data.bldblp);
-                sqlCommand.Parameters.AddWithValue("@summary", args.data.summary);
-                sqlCommand.ExecuteNonQuery();
-
-                sqlConnection.Close();
-
-            }
-
-            private static string GetConnectionStringByName(string name)
-            {
-                // Assume failure.
-                string returnValue = null;
-
-                // Look for the name in the connectionStrings section.
-                ConnectionStringSettings settings =
-                    ConfigurationManager.ConnectionStrings[name];
-
-                // If found, return the connection string.
-                if (settings != null)
-                    returnValue = settings.ConnectionString;
-
-                return returnValue;
-            }
-        }
     }
 }
